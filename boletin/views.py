64fdb9d0fbf17026f68ea2585c3b96d02a3ce712
unknown <drmdfoureilu@gmail.com>
2018-02-26 13:15:43 -0600

    ._.

--- boletin/views.py
+++ boletin/views.py
@@ -1,22 +1,18 @@
 from django.shortcuts import render
-from .forms import RegForm,ModelRegForm
+from .forms import RegForm
 from .models import Registrado
 def inicio(request):
 	titulo="Hola"
 	if request.user.is_authenticated():
-		titulo="Bienvenido %s" %(request.user)
-	form=ModelRegForm(request.POST or None)
+		titulo="Bienvenido %s" % (request.user)
+	form=RegForm(request.POST or None)
+	
 	if form.is_valid():
-		instance=form.save(commit=False)
-		instance.save()
-
-		# form_data=form.cleaned_data
-		# abc=form_data.get("email")
-		# abc2=form_data.get("nombre")
-		# obj=Registrado.objects.create(email=abc,nombre=abc2)
+		form_data=form.cleaned_data
+		abc=form_data.get("email")
+		abc2=form_data.get("nombre")
+		obj=Registrado.objects.create(email=abc,nombre=abc2)
 	contexto={
-		"titulo":titulo,
-		"abc":abc,
 		"mi_formulario":form,
 	}
 	return render(request,"inicio.html",contexto)
