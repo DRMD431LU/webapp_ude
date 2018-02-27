bcfeb34b6e85100d1ac688aaae06134924d23d28
unknown <drmdfoureilu@gmail.com>
2018-02-27 12:21:56 -0600

    metodo GET :)

--- boletin/views.py
+++ boletin/views.py
@@ -2,19 +2,9 @@ from django.shortcuts import render
 from .forms import RegForm
 from .models import Registrado
 def inicio(request):
-	titulo="Hola"
-	if request.user.is_authenticated():
-		titulo="Bienvenido %s" % (request.user)
-
-	form=RegForm(request.POST or None)
-	if form.is_valid():
-		form_data=form.cleaned_data
-		abc=form_data.get("email")
-		abc2=form_data.get("nombre")
-		obj=Registrado.objects.create(email=abc,nombre=abc2)
+	form=RegForm()
 	contexto={
 		"mi_formulario":form,
-		"titulo":titulo,
 	}
 	return render(request,"inicio.html",contexto)
 # Create your views here.
