671a2a98442b696a9b1151aca9f23a434c782faf
unknown <drmdfoureilu@gmail.com>
2018-02-27 11:12:38 -0600

    :'v no idea

--- boletin/views.py
+++ boletin/views.py
@@ -5,8 +5,8 @@ def inicio(request):
 	titulo="Hola"
 	if request.user.is_authenticated():
 		titulo="Bienvenido %s" % (request.user)
+
 	form=RegForm(request.POST or None)
-	
 	if form.is_valid():
 		form_data=form.cleaned_data
 		abc=form_data.get("email")
@@ -14,6 +14,7 @@ def inicio(request):
 		obj=Registrado.objects.create(email=abc,nombre=abc2)
 	contexto={
 		"mi_formulario":form,
+		"titulo":titulo,
 	}
 	return render(request,"inicio.html",contexto)
 # Create your views here.
