845e8075909f4f5d85fd144cd35b8fd9bc6d8dca
unknown <drmdfoureilu@gmail.com>
2018-02-28 13:37:24 -0600

    crispy forms

--- webapp_udemy/settings.py
+++ webapp_udemy/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'boletin'
+    'crispy_forms',
+    'boletin',
 ]
 
 MIDDLEWARE = [
@@ -124,5 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR,"static_pro","static"),]
 STATIC_ROOT= os.path.join(os.path.dirname(BASE_DIR),"static_env","static_root")
 MEDIA_ROOT=os.path.join(os.path.dirname(BASE_DIR),"static_env","media_root")
